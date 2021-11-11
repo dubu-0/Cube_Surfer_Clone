e167f1d8148a7cb915d0f9e89c22a89bdcb5eb90
dubu-0 <yudin01@mail.ru>
2021-11-11 18:18:50 +0300

    Ограничил движение игрока влево-вправо, чтобы он не вылетал за пределы дорожки

--- Assets/PlayerStrafe.cs
+++ Assets/PlayerStrafe.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 public class PlayerStrafe : MonoBehaviour
 {
     [SerializeField] private float speed = 15;
+    [SerializeField] private Track track;
 
     private const string Horizontal = nameof(Horizontal);
 
     private void LateUpdate()
     {
-        Strafe(Input.GetAxis(Horizontal));
+        Strafe(Input.GetAxis(Horizontal), track.GetBoundaries());
     }
 
-    private void Strafe(float direction)
+    private void Strafe(float direction, (float, float) boundaries)
     {
-        var newPos = new Vector3(speed * Time.deltaTime, 0, 0);
-
-        if (direction != 0)
-            transform.localPosition += Mathf.Sign(direction) * newPos;
+        if (direction == 0) return;
+        
+        var step = new Vector3(speed * Time.deltaTime, 0, 0);
+        var newPos = Mathf.Sign(direction) * step + transform.localPosition;
+        var clampedX = Mathf.Clamp(newPos.x, boundaries.Item1 + 0.5f, boundaries.Item2 - 0.5f);
+        transform.localPosition = new Vector3(clampedX, newPos.y, newPos.z);
     }
 }
